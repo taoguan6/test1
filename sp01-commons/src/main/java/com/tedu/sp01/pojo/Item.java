7a3fedacd4c41aa3df0f5b173d7d2ee48c52d36d
Administrator <Administrator@USER-20161013UJ>
2019-08-27 19:47:37 +0800

    1

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,13 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class Item {
+	private Integer id;
+	private String name;
+	private Integer number;
+}
---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
@@ -0,0 +1,13 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class User {
+	private Integer id;
+	private String username;
+	private String password;
+}
